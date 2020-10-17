b6bbf9910dcb94416caad0ae10345a17e390167a
David Luque Callejas <david_junior@live.com>
2020-10-17 04:18:44 +0200

    Revert "test"
    This reverts commit 205e943858c53d9789a08c6ee5e02fdc70aa302f.

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ async function run() {
     const taskDefinitionFile = core.getInput('task-definition', { required: true });
     const containerName = core.getInput('container-name', { required: true });
     const imageURI = core.getInput('image', { required: true });
-    const environmentVarName = core.getInput('environment-variable-name', { required: false })
-    const environmentVarValue = core.getInput('environment-variable-value', { required: false })
 
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
@@ -32,17 +30,6 @@ async function run() {
       throw new Error('Invalid task definition: Could not find container definition with matching name');
     }
     containerDef.image = imageURI;
-
-    let obj = containerDef.environment.find((o, i) => {
-      if (o.name === environmentVarName) {
-        containerDef.environment[i] = { name: environmentVarName, value: environmentVarValue };
-          return true; // stop searching
-      }
-    });
-
-      if (obj == undefined){
-        containerDef.environment.push({name: environmentVarName, value: environmentVarValue})
-      }
 
     // Write out a new task definition file
     var updatedTaskDefFile = tmp.fileSync({
